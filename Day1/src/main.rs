500a718c4f00aa4bfa50baaf094ef84590fcd89a
ceron257 <Ceron257@users.noreply.github.com>
2022-12-01 22:02:58 +0100

    Day 1

--- Day1/src/main.rs
+++ Day1/src/main.rs
@@ -1,3 +1,32 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::vec::Vec;
+
+fn read_input<P>(file_name : P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>,
+{
+  let file = File::open(file_name)?;
+  Ok(io::BufReader::new(file).lines())
+}
+
 fn main() {
-    println!("Hello, world!");
+  if let Ok(lines) = read_input("input.txt") {
+    let mut current_sum : u32 = 0;
+    let mut sums : Vec<u32> = Vec::new();
+    for line_result in lines {
+      let line = line_result.expect("Cannot read line");
+      if line.trim().is_empty() {
+        sums.push(current_sum);
+        current_sum = 0;
+      }
+      else {
+          current_sum += line.parse::<u32>().expect("Cannot parse.");
+      }
+    }
+    sums.sort();
+    println!("Max = {}", sums.iter().max().unwrap());
+    let top_three : u32 = sums.iter().rev().take(3).sum();
+    println!("Part 2 = {}", top_three);
+  }
 }
