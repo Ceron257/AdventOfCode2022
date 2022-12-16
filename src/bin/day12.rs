34c8ffd951d138156be6217bb418a53eb3b70a05
ceron257 <Ceron257@users.noreply.github.com>
2022-12-16 23:35:15 +0100

    Finish day 12

--- src/bin/day12.rs
+++ src/bin/day12.rs
@@ -1,11 +1,11 @@
-use std::slice::Iter;
+use std::{collections::HashMap, slice::Iter};
 use utilities::read_input;
 
 type Value = usize;
 type Row = Vec<Value>;
 type Position = (i32, i32);
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 struct Heightmap {
     rows: Vec<Row>,
     start: Option<Position>,
@@ -51,8 +51,8 @@ impl Heightmap {
             .filter(|p| {
                 p.0 >= 0
                     && p.1 >= 0
-                    && p.0 < self.rows[0].len() as i32
-                    && p.1 < self.rows.len() as i32
+                    && p.0 < self.rows.len() as i32
+                    && p.1 < self.rows[0].len() as i32
             })
             .map(|p| *p)
             .collect::<Vec<_>>();
@@ -63,8 +63,96 @@ impl Heightmap {
         self.rows[position.0 as usize][position.1 as usize]
     }
 
+    fn heuristic(from: Position, to: Position) -> usize {
+        ((to.0 - from.0).abs() + (to.1 - from.1).abs()) as usize
+    }
+
+    fn reconstruct_path(
+        predecessors: HashMap<Position, Option<Position>>,
+        finish: Position,
+    ) -> Vec<Position> {
+        let mut path = Vec::new();
+        let mut current = finish;
+        path.push(current);
+        while let Some(predecessor) = predecessors.get(&current) {
+            path.insert(0, predecessor.unwrap());
+            current = predecessor.unwrap();
+        }
+        path
+    }
+
     fn shortest_path(&self, from: Position, to: Position) -> Vec<Position> {
+        let mut open_nodes = Vec::new();
+        let mut closed_nodes = Vec::new();
+
+        let mut path = HashMap::new();
+
+        open_nodes.push((from, 0));
+        loop {
+            let current_node = open_nodes
+                .iter()
+                .min_by(|x, y| x.1.cmp(&y.1))
+                .unwrap()
+                .clone();
+            open_nodes.remove(open_nodes.iter().position(|&x| x == current_node).unwrap());
+            if current_node.0 == to {
+                return Heightmap::reconstruct_path(path, self.finish.unwrap());
+            }
+            closed_nodes.push(current_node.0);
+            for neighbor in self.neighbors(current_node.0) {
+                if closed_nodes.contains(&neighbor) {
+                    continue;
+                }
+                let height_difference = self
+                    .height(neighbor)
+                    .saturating_sub(self.height(current_node.0));
+                if height_difference > 1 {
+                    continue;
+                }
+                let tentative_cost = current_node.1 + height_difference;
+                // if we have not found a  better alternative just continue:
+                if !open_nodes.is_empty()
+                    && open_nodes.iter().any(|(position, _)| *position == neighbor)
+                    && !open_nodes.iter().any(|(position, distance)| {
+                        position == &neighbor && *distance > tentative_cost
+                    })
+                {
+                    continue;
+                }
+                *path.entry(neighbor).or_insert(None) = Some(current_node.0);
+                let estimated_cost = tentative_cost
+                    + Heightmap::heuristic(neighbor, self.finish.expect("Got no finish"));
+                if let Some(existing_index) = open_nodes
+                    .iter()
+                    .position(|(position, _)| *position == neighbor)
+                {
+                    open_nodes.remove(existing_index);
+                }
+                open_nodes.push((neighbor, estimated_cost));
+            }
+            if open_nodes.is_empty() {
+                break;
+            }
+        }
         Vec::new()
+    }
+
+    fn shortest_hiking_trail(self) -> Vec<Position> {
+        let finish = self.finish.unwrap();
+        let mut path = self.shortest_path(self.start.unwrap(), finish);
+        for row in 0..self.rows.len() {
+            for column in 0..self.rows[0].len() {
+                let position = (row as i32, column as i32);
+                if self.height(position) != 0 {
+                    continue;
+                }
+                let possibly_shorter_path = self.shortest_path(position, finish);
+                if !possibly_shorter_path.is_empty() && possibly_shorter_path.len() < path.len() {
+                    path = possibly_shorter_path;
+                }
+            }
+        }
+        path
     }
 }
 
@@ -94,7 +182,22 @@ fn find_finish_in_row(row: &str) -> Option<i32> {
 fn main() {
     if let Ok(lines) = read_input("inputs/day12.txt") {
         let heightmap = Heightmap::new(lines.iter());
-        println!("{:?}", heightmap);
+        println!(
+            "The shortest path takes {:?} steps.",
+            heightmap
+                .clone()
+                .shortest_path(
+                    heightmap.start.expect("Got no start."),
+                    heightmap.finish.expect("Got no finish.")
+                )
+                .len()
+                .saturating_sub(1)
+        );
+        let shortest_trail = heightmap.shortest_hiking_trail();
+        println!(
+            "The shortest hiking trail takes {:?} steps.",
+            shortest_trail.len().saturating_sub(1)
+        );
     } else {
         println!("Couldn't read input.");
     }
